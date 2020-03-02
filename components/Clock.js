b6ae2cea24be413f66bad4ebfdb1374423312448
PrzemekSobolewski <przemus212@gmail.com>
2020-03-02 21:30:24 +0100

    zone current Time

--- components/Clock.js
+++ components/Clock.js
@@ -1,30 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useSelector} from 'react-redux'
 
 const Clock = () => {
     const [time, setTime] = useState(null);
-    const [offset, setOffset] = useState(null);
     const weather = useSelector(state => state.myWeather);
 
-    setInterval(() => {
-        let currentTime = new Date();
-        setOffset((weather.timezone / 3600) + (currentTime.getTimezoneOffset() / 60));
-        let hours = currentTime.getHours() + offset;
-        if (hours > 24) hours = hours - 24;
-        if (hours < 0) hours = Math.abs(hours);
-        let minutes = currentTime.getMinutes();
+    useEffect(() => {
+        const interval =  setInterval(() => {
+            console.log(weather.timezone);
+            let currentTime = new Date();
+            let offset = (weather.timezone / 3600) + (currentTime.getTimezoneOffset() / 60);
+            let hours = currentTime.getHours() + offset;
+            if (hours > 24) hours = hours - 24;
+            if (hours < 0) hours = Math.abs(hours);
+            let minutes = currentTime.getMinutes();
 
-        if (minutes < 10) {
-            minutes = "0" + minutes
-        }
-        let zoneTime = hours + ":" + minutes + " ";
-        if (hours > 11) {
-            zoneTime += "PM";
-        } else {
-            zoneTime += "AM";
-        }
-        setTime(zoneTime)
-    }, 200);
+            if (minutes < 10) {
+                minutes = "0" + minutes
+            }
+            let zoneTime = hours + ":" + minutes + " ";
+            if (hours > 11) {
+                zoneTime += "PM";
+            } else {
+                zoneTime += "AM";
+            }
+            setTime(zoneTime)
+        }, 200);
+        return () => clearInterval(interval);
+    }, [weather.timezone]);
 
     return (
         <div>
--- components/Region.js
+++ components/Region.js
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 import {css} from 'emotion';
 import {IoIosSearch} from "react-icons/io"
 import Clock from './Clock';
+import {useCookies, CookiesProvider} from 'react-cookie';
 
 const Icon = css({
     fontSize: "2.5em",
@@ -12,7 +13,7 @@ const Icon = css({
     marginLeft: "-1.3em",
     float: "left",
     color: "white",
-    ':hover':{
+    ':hover': {
         cursor: "pointer",
         color: "#ff8080"
     }
@@ -54,18 +55,21 @@ const LocalTime = css({
 
 const Region = () => {
     const weather = useSelector(state => state.myWeather);
-    const [city, setCity] = useState(weather.city);
+    const [city, setCity] = useState(null);
     const dispatch = useDispatch();
+    const [cookies, setCookie] = useCookies(['city']);
 
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(startFetchingWeather)
-        } else {
-            alert("Geolocation not supported on this browser")
-        }
+    const getLocationByCookies = () => {
+        dispatch(actions.fetchWeatherFromCity(cookies.city));
     };
 
-    const startFetchingWeather = (position) => dispatch(actions.fetchWeatherFromLocation(position.coords.latitude,position.coords.longitude));
+    const getLocation = () => {
+        if (cookies.city !== undefined) getLocationByCookies();
+        else if (navigator.geolocation) navigator.geolocation.getCurrentPosition(startFetchingWeather)
+        else alert("Geolocation not supported on this browser")
+    };
+
+    const startFetchingWeather = (position) => dispatch(actions.fetchWeatherFromLocation(position.coords.latitude, position.coords.longitude));
 
     useEffect(getLocation, []);
 
@@ -74,21 +78,25 @@ const Region = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         dispatch(actions.fetchWeatherFromCity(city));
+        setCookie('city', city, {path: '/'});
     };
 
     return (
-        <div style={{display: "grid"}}>
-            <div className={RegionSelector}>
-                <RegionInput value={city} type={"text"} name={"city"} id={"city"} required
-                             minlength="1" maxlength="15"
-                       onChange={handleChange}/>
-                <IoIosSearch className={Icon} onClick={handleSubmit} />
+        <CookiesProvider>
+            <div style={{display: "grid"}}>
+                <div className={RegionSelector}>
+                    <RegionInput value={city} type={"text"} name={"city"}
+                                 id={"city"} required
+                                 minlength="1" maxlength="15"
+                                 onChange={handleChange}/>
+                    <IoIosSearch className={Icon} onClick={handleSubmit}/>
 
+                </div>
+                <div className={LocalTime}>
+                    <Clock/>
+                </div>
             </div>
-            <div className={LocalTime}>
-                <Clock />
-            </div>
-        </div>
+        </CookiesProvider>
     )
 
 };
